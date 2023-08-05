9c13df8f069d2fcd27d711c988b15f7f54194dd1
Doni Mustasar <donny.md17@gmail.com>
2023-08-05 17:41:17 +0700

    Add table `question`

---
+++ app/Models/Question.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Scopes\DataGridScope;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Question extends Model
+{
+    use HasFactory;
+
+    public function scopeFilter($query)
+    {
+        return (new DataGridScope())->apply($query, $this);
+    }
+
+    protected $table        = 'questions';
+    protected $primaryKey   = 'id';
+    // protected $keyType      = 'string';
+    // public $incrementing    = false;
+    protected $fillable = [
+        'survey_id',
+        'question_text',
+    ];
+
+    public function survey()
+    {
+        return $this->belongsTo(Survey::class);
+    }
+
+    public function options()
+    {
+        return $this->hasMany(QuestionOption::class);
+    }
+}
