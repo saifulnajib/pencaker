e8f9b374d09cf86d2d4d68949412615b5e241fcf
Doni Mustasar <donny.md17@gmail.com>
2023-08-15 16:59:52 +0700

    Add table `bank_sampah`

---
+++ app/Models/BankSampah.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Scopes\DataGridScope;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BankSampah extends Model
+{
+    use HasFactory;
+
+    public function scopeFilter($query)
+    {
+        return (new DataGridScope())->apply($query, $this);
+    }
+
+    protected $table        = 'bank_sampah';
+    protected $primaryKey   = 'id';
+    // protected $keyType      = 'string';
+    // public $incrementing    = false;
+    protected $fillable = [
+        'nama', 'alamat',
+        'kode_wilayah', 'file_surat_pengajuan',
+        'status_pengajuan', 'is_active'
+    ];
+}
